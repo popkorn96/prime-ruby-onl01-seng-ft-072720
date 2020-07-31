dc78d834983e5f3a0dbbd5d040883aa145a2ec14
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-07-31 19:08:58 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,6 +3,6 @@ def prime?(number)
   n = number
   return false if n < 2
   return true if 2 == 2
-(2..number-1).each {|n| return false if n % i == 0 }
+(2..number-1).each {|int| return false if number % i == 0 }
 true
 end
